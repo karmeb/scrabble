d77436b1ef5be05b76c1187383fbbfa7311157c7
unknown <karme.barg@gmail.com>
2024-12-08 13:15:46 +0200

    replaced commandLineRunner with ApplicationRunner, replaced css with scss

--- scrabble-backend/src/main/java/com/karme/scrabblebackend/config/WordDataInitializer.java
+++ scrabble-backend/src/main/java/com/karme/scrabblebackend/config/WordDataInitializer.java
@@ -3,19 +3,20 @@ package com.karme.scrabblebackend.config;
 import com.karme.scrabblebackend.service.ScrabbleWordService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.ApplicationArguments;
+import org.springframework.boot.ApplicationRunner;
 import org.springframework.stereotype.Component;
 
 @Component
 @RequiredArgsConstructor
-public class WordDataInitializer implements CommandLineRunner {
+public class WordDataInitializer implements ApplicationRunner {
     private final ScrabbleWordService scrabbleWordService;
 
     @Value("${file.words.path}")
     private String filePath;
 
     @Override
-    public void run(String... args) {
+    public void run(ApplicationArguments args) {
         scrabbleWordService.saveWordsFromFile(filePath);
     }
 }
