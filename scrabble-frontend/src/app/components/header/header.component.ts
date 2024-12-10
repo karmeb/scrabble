8ae0aba15733a6888ec377ea9d5efed2f57e392e
unknown <karme.barg@gmail.com>
2024-12-10 09:07:18 +0200

    changed header button to update dynamically based on current page

--- scrabble-frontend/src/app/components/header/header.component.ts
+++ scrabble-frontend/src/app/components/header/header.component.ts
@@ -1,5 +1,5 @@
-import { Component, signal } from '@angular/core';
-import { RouterLink } from '@angular/router';
+import { Component, OnInit, signal, inject } from '@angular/core';
+import { Router, RouterLink, NavigationEnd } from '@angular/router';
 
 @Component({
   selector: 'app-header',
@@ -7,7 +7,29 @@ import { RouterLink } from '@angular/router';
   templateUrl: './header.component.html',
   styleUrl: './header.component.scss'
 })
-export class HeaderComponent {
+export class HeaderComponent implements OnInit {
+  router = inject(Router)
   title = signal('Scrabble Words Helper')
+  routPath = signal<string>('')
+
+  navigationButtonText: { [key: string]: string } = {
+    '/': 'Check Words',
+    '/add-word': 'Add New Word',
+  };
+
+  ngOnInit() {
+    this.router.events.subscribe(
+      (event: any) => {
+        if (event instanceof NavigationEnd) {
+          let currentUrl = this.router.url;
+          if (currentUrl === "/") {
+            this.routPath.set("/add-word")
+          } else {
+            this.routPath.set("/")
+          }
+        }
+      }
+    );
+  }
 
 }
